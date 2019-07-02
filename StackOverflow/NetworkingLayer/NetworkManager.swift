b0f17b7608bc4ff669c3ef93bcc87b2995692426
amith <amitkumarc@triconinfotech.com>
2019-07-03 05:08:55 +0530

    Alamofire integrated

--- StackOverflow/NetworkingLayer/NetworkManager.swift
+++ StackOverflow/NetworkingLayer/NetworkManager.swift
@@ -5,22 +5,6 @@ struct NetworkManager {
     private let router = Router<StackExchangeAPI>()
     let sessionManager = Alamofire.SessionManager.default
 
-//    func getAllQuestions (completion: @escaping (AllQuestions?, Error?) -> Void) {
-//        router.request (.allQuestions) { data, _, error in
-//            //print(data ?? "noData")
-//            //print(response ?? "noResponse")
-//            print(error ?? "noError")
-//            do {
-//                let decoder = JSONDecoder().convertFromSnakeCase()
-//                let decodedData = try decoder.decode(AllQuestions.self, from: data!)
-//                completion(decodedData, nil)
-//            } catch {
-//                print(error.localizedDescription)
-//                completion(nil, error)
-//            }
-//        }
-//    }
-
     func getAllQuestions (completion: @escaping (AllQuestions?, Error?) -> Void) {
         do {
             let urlRequest = try router.buildRequest(from: .allQuestions)
@@ -36,7 +20,57 @@ struct NetworkManager {
                     print("error: \(e.localizedDescription)")
                     completion(nil, e)
                 }
+            }
+        }
+        catch (let err) {
+            print("error: \(err.localizedDescription)")
+            completion(nil, err)
+        }
+    }
 
+
+    func getQuestion(with qId: Int, completion: @escaping (Question?, Error?) -> Void) {
+
+        do {
+            let urlRequest = try router.buildRequest(from: .question(id: qId))
+            sessionManager.request(urlRequest)
+                .validate()
+                .responseData { response in
+                    guard response.error == nil, let data = response.data else { return }
+                    let decoder = JSONDecoder().convertFromSnakeCase()
+                    do {
+                        let decodedData = try decoder.decode(Question.self, from: data)
+                        completion(decodedData, nil)
+                    } catch (let e) {
+                        print("error: \(e.localizedDescription)")
+                        completion(nil, e)
+                    }
+            }
+        }
+        catch (let err) {
+            print("error: \(err.localizedDescription)")
+            completion(nil, err)
+        }
+    }
+
+
+
+    func getAnswersOfQuestion(with qId: Int, completion: @escaping (Answers?, Error?) -> Void) {
+
+        do {
+            let urlRequest = try router.buildRequest(from: .answersOfQuestion(id: qId))
+            sessionManager.request(urlRequest)
+                .validate()
+                .responseData { response in
+                    guard response.error == nil, let data = response.data else { return }
+                    let decoder = JSONDecoder().convertFromSnakeCase()
+                    do {
+                        let decodedData = try decoder.decode(Answers.self, from: data)
+                        completion(decodedData, nil)
+                    } catch (let e) {
+                        print("error: \(e.localizedDescription)")
+                        completion(nil, e)
+                    }
             }
         }
         catch (let err) {
@@ -46,56 +80,27 @@ struct NetworkManager {
 
     }
 
-
-    func getQuestion(with qId: Int, completion: @escaping (Question?, Error?) -> Void) {
-        router.request(.question(id: qId)) { data, _, error in
-            guard error == nil else {
-                return
-            }
-            do {
-                let decoder = JSONDecoder().convertFromSnakeCase()
-                let decodedData = try decoder.decode(Question.self, from: data!)
-                completion(decodedData, nil)
-            } catch {
-                print(error.localizedDescription)
-                completion(nil, error)
-            }
-        }
-    }
-
-    func getAnswersOfQuestion(with qId: Int, completion: @escaping (Answers?, Error?) -> Void) {
-        router.request(.answersOfQuestion(id: qId)) { data, _, error in
-            print(error ?? "noError")
-            guard data != nil else {
-                print("data not found: getAnswersOfQuestion")
-                return
-            }
-            do {
-                let decoder = JSONDecoder().convertFromSnakeCase()
-                let decodedData = try decoder.decode(Answers.self, from: data!)
-                completion(decodedData, nil)
-            } catch {
-                print(error.localizedDescription)
-                completion(nil, error)
-            }
-        }
-    }
-
     func getUser(with userID: Int, completion: @escaping (User?, Error?) -> Void) {
-        router.request(.user(id: userID)) { data, _, error in
-            print(error ?? "noError")
-            guard data != nil else {
-                print("data not found: getAnswersOfQuestion")
-                return
+
+        do {
+            let urlRequest = try router.buildRequest(from: .user(id: userID))
+            sessionManager.request(urlRequest)
+                .validate()
+                .responseData { response in
+                    guard response.error == nil, let data = response.data else { return }
+                    let decoder = JSONDecoder().convertFromSnakeCase()
+                    do {
+                        let decodedData = try decoder.decode(User.self, from: data)
+                        completion(decodedData, nil)
+                    } catch (let e) {
+                        print("error: \(e.localizedDescription)")
+                        completion(nil, e)
+                    }
             }
-            do {
-                let decoder = JSONDecoder().convertFromSnakeCase()
-                let decodedData = try decoder.decode(User.self, from: data!)
-                completion(decodedData, nil)
-            } catch {
-                print(error.localizedDescription)
-                completion(nil, error)
-            }
+        }
+        catch (let err) {
+            print("error: \(err.localizedDescription)")
+            completion(nil, err)
         }
     }
 }
